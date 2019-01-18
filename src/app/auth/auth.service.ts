de8c60edbf853dce42b8f4d11fd852d8cea31691
COSTAISAORG\CCU <cmunozlopez@costaisa.com>
2019-01-18 10:45:04 +0100

    Sección 8 lista - inicio redux

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,25 +8,45 @@ import { map } from 'rxjs/operators';
 import Swal from 'sweetalert2';
 import { User } from './user.model';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
+import { SetUserAction } from './auth.actions';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private userSubscription: Subscription = new Subscription();
+
   constructor(
     private afAuth: AngularFireAuth,
     private router: Router,
-    private afDB: AngularFirestore
+    private afDB: AngularFirestore,
+    private store: Store<AppState>
   ) { }
 
   initAuthListener() {
-    this.afAuth.authState.subscribe((fbUser: firebase.User) => {
-      console.log(fbUser);
+    this.userSubscription = this.afAuth.authState.subscribe((fbUser: firebase.User) => {
+      if(fbUser) {
+        this.afDB.doc(`${ fbUser.uid }/usuario`)
+          .valueChanges()
+          .subscribe((usuarioObj: any) => {
+            const newUser = new User(usuarioObj);
+            this.store.dispatch(new SetUserAction(newUser));
+          });
+      } else {
+        this.userSubscription.unsubscribe();
+      }
     });
   }
 
   crearUsuario(nombre: string, email: string, password: string) {
+
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth
       .createUserWithEmailAndPassword(email, password)
       .then(resp => {
@@ -41,6 +61,7 @@ export class AuthService {
           .set(user)
           .then(() => {
             this.router.navigate(['/']);
+            this.store.dispatch(new DesactivarLoadingAction());
           })
       })
       .catch(error => {
@@ -49,8 +70,12 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
+
+    this.store.dispatch(new ActivarLoadingAction());
+
     this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then(resp => {
+        this.store.dispatch(new DesactivarLoadingAction());
         this.router.navigate(['/']);
       })
       .catch(error => {
